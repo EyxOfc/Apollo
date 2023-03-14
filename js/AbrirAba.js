b858d89c242c9b073a3beec035ae032991a11227
EyxOfc <108961402+EyxOfc@users.noreply.github.com>
2023-03-14 00:20:56 -0300

    Up

---
+++ js/AbrirAba.js
@@ -0,0 +1,8 @@
+var Btn = document.querySelector('.Button');
+var Aba = document.querySelector('#Aba');
+var body = document.querySelector('body');
+
+function AbrirAba(){
+    Aba.style.display = 'flex';
+    body.style.overflow = 'hidden';
+}
---
+++ js/FecharAba.js
@@ -0,0 +1,8 @@
+var Btn = document.querySelector('.Button');
+var Aba = document.querySelector('#Aba');
+var body = document.querySelector('body');
+
+function FecharAba(){
+    Aba.style.display = 'none';
+    body.style.overflow = 'scroll';
+}
